c3d184f2eec0bfbe9a1f04790843cde9c8dd357b
Adam Sołtysik <adam27.sol@gmail.com>
2022-06-10 22:47:38 +0200

    Release version 2.0.0

--- dist/vue-pocket-router.js
+++ dist/vue-pocket-router.js
@@ -1,213 +1,54 @@
 (function (global, factory) {
-    typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory(require('url-pattern')) :
-    typeof define === 'function' && define.amd ? define(['url-pattern'], factory) :
-    (global = typeof globalThis !== 'undefined' ? globalThis : global || self, global.VuePocketRouter = factory(global.UrlPattern));
-}(this, (function (UrlPattern) { 'use strict';
+    typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('vue'), require('url-pattern')) :
+    typeof define === 'function' && define.amd ? define(['exports', 'vue', 'url-pattern'], factory) :
+    (global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global.VuePocketRouter = {}, global.Vue, global.UrlPattern));
+})(this, (function (exports, vue, UrlPattern) { 'use strict';
 
     function _interopDefaultLegacy (e) { return e && typeof e === 'object' && 'default' in e ? e : { 'default': e }; }
 
     var UrlPattern__default = /*#__PURE__*/_interopDefaultLegacy(UrlPattern);
 
-    //
-    //
-    //
-    //
-    //
-    //
-    //
-
-    var script = {
-        props: {
-            to: { type: String, required: true },
-        },
-        methods: {
-            navigate(event) {
-                this.$router.push(this.to);
-                event.preventDefault();
+    var script$1 = {
+            props: {
+                to: { type: String, required: true },
             },
-        },
-    };
-
-    function normalizeComponent(template, style, script, scopeId, isFunctionalTemplate, moduleIdentifier
-    /* server only */
-    , shadowMode, createInjector, createInjectorSSR, createInjectorShadow) {
-      if (typeof shadowMode !== 'boolean') {
-        createInjectorSSR = createInjector;
-        createInjector = shadowMode;
-        shadowMode = false;
-      } // Vue.extend constructor export interop.
-
-
-      var options = typeof script === 'function' ? script.options : script; // render functions
-
-      if (template && template.render) {
-        options.render = template.render;
-        options.staticRenderFns = template.staticRenderFns;
-        options._compiled = true; // functional template
-
-        if (isFunctionalTemplate) {
-          options.functional = true;
-        }
-      } // scopedId
-
-
-      if (scopeId) {
-        options._scopeId = scopeId;
-      }
-
-      var hook;
-
-      if (moduleIdentifier) {
-        // server build
-        hook = function hook(context) {
-          // 2.3 injection
-          context = context || this.$vnode && this.$vnode.ssrContext || this.parent && this.parent.$vnode && this.parent.$vnode.ssrContext; // functional
-          // 2.2 with runInNewContext: true
-
-          if (!context && typeof __VUE_SSR_CONTEXT__ !== 'undefined') {
-            context = __VUE_SSR_CONTEXT__;
-          } // inject component styles
-
-
-          if (style) {
-            style.call(this, createInjectorSSR(context));
-          } // register component module identifier for async chunk inference
-
-
-          if (context && context._registeredComponents) {
-            context._registeredComponents.add(moduleIdentifier);
-          }
-        }; // used by ssr in case component is cached and beforeCreate
-        // never gets called
-
-
-        options._ssrRegister = hook;
-      } else if (style) {
-        hook = shadowMode ? function (context) {
-          style.call(this, createInjectorShadow(context, this.$root.$options.shadowRoot));
-        } : function (context) {
-          style.call(this, createInjector(context));
+            methods: {
+                navigate(event) {
+                    this.$router.push(this.to);
+                    event.preventDefault();
+                },
+            },
         };
-      }
 
-      if (hook) {
-        if (options.functional) {
-          // register for functional component in vue file
-          var originalRender = options.render;
+    const _hoisted_1 = ["href"];
 
-          options.render = function renderWithStyleInjection(h, context) {
-            hook.call(context);
-            return originalRender(h, context);
-          };
-        } else {
-          // inject component registration as beforeCreate hook
-          var existing = options.beforeCreate;
-          options.beforeCreate = existing ? [].concat(existing, hook) : [hook];
-        }
-      }
-
-      return script;
+    function render$1(_ctx, _cache, $props, $setup, $data, $options) {
+      return (vue.openBlock(), vue.createElementBlock("a", {
+        href: $props.to,
+        onClick: _cache[0] || (_cache[0] = (...args) => ($options.navigate && $options.navigate(...args)))
+      }, [
+        vue.renderSlot(_ctx.$slots, "default")
+      ], 8 /* PROPS */, _hoisted_1))
     }
 
-    /* script */
-    const __vue_script__ = script;
+    script$1.render = render$1;
+    script$1.__file = "src/components/router-link.vue";
 
-    /* template */
-    var __vue_render__$1 = function() {
-      var _vm = this;
-      var _h = _vm.$createElement;
-      var _c = _vm._self._c || _h;
-      return _c(
-        "a",
-        { attrs: { href: _vm.to }, on: { click: _vm.navigate } },
-        [_vm._t("default")],
-        2
-      )
-    };
-    var __vue_staticRenderFns__$1 = [];
-    __vue_render__$1._withStripped = true;
+    function render(_ctx, _cache) {
+      return (vue.openBlock(), vue.createBlock(vue.resolveDynamicComponent(_ctx.$route.component), vue.mergeProps({
+        key: _ctx.$route.key
+      }, { ..._ctx.$route.params, ..._ctx.$route.props }), null, 16 /* FULL_PROPS */))
+    }
 
-      /* style */
-      const __vue_inject_styles__$1 = undefined;
-      /* scoped */
-      const __vue_scope_id__$1 = undefined;
-      /* module identifier */
-      const __vue_module_identifier__$1 = undefined;
-      /* functional template */
-      const __vue_is_functional_template__$1 = false;
-      /* style inject */
-      
-      /* style inject SSR */
-      
-      /* style inject shadow dom */
-      
+    const script = {};
 
-      
-      const __vue_component__$1 = /*#__PURE__*/normalizeComponent(
-        { render: __vue_render__$1, staticRenderFns: __vue_staticRenderFns__$1 },
-        __vue_inject_styles__$1,
-        __vue_script__,
-        __vue_scope_id__$1,
-        __vue_is_functional_template__$1,
-        __vue_module_identifier__$1,
-        false,
-        undefined,
-        undefined,
-        undefined
-      );
 
-    /* script */
+    script.render = render;
+    script.__file = "src/components/router-view.vue";
 
-    /* template */
-    var __vue_render__ = function() {
-      var _vm = this;
-      var _h = _vm.$createElement;
-      var _c = _vm._self._c || _h;
-      return _c(
-        _vm.$route.component,
-        _vm._b(
-          { key: _vm.$route.key, tag: "component" },
-          "component",
-          Object.assign({}, _vm.$route.params, _vm.$route.props),
-          false
-        )
-      )
-    };
-    var __vue_staticRenderFns__ = [];
-    __vue_render__._withStripped = true;
+    function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); enumerableOnly && (symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; })), keys.push.apply(keys, symbols); } return keys; }
 
-      /* style */
-      const __vue_inject_styles__ = undefined;
-      /* scoped */
-      const __vue_scope_id__ = undefined;
-      /* module identifier */
-      const __vue_module_identifier__ = undefined;
-      /* functional template */
-      const __vue_is_functional_template__ = false;
-      /* style inject */
-      
-      /* style inject SSR */
-      
-      /* style inject shadow dom */
-      
-
-      
-      const __vue_component__ = /*#__PURE__*/normalizeComponent(
-        { render: __vue_render__, staticRenderFns: __vue_staticRenderFns__ },
-        __vue_inject_styles__,
-        {},
-        __vue_scope_id__,
-        __vue_is_functional_template__,
-        __vue_module_identifier__,
-        false,
-        undefined,
-        undefined,
-        undefined
-      );
-
-    function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) { symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); } keys.push.apply(keys, symbols); } return keys; }
-
-    function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
+    function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = null != arguments[i] ? arguments[i] : {}; i % 2 ? ownKeys(Object(source), !0).forEach(function (key) { _defineProperty(target, key, source[key]); }) : Object.getOwnPropertyDescriptors ? Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)) : ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } return target; }
 
     function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
@@ -215,12 +56,10 @@
 
     function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
 
-    function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+    function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); Object.defineProperty(Constructor, "prototype", { writable: false }); return Constructor; }
 
     var Router = /*#__PURE__*/function () {
       function Router(_ref) {
-        var _this = this;
-
         var routes = _ref.routes,
             _ref$base = _ref.base,
             base = _ref$base === void 0 ? '' : _ref$base;
@@ -233,22 +72,31 @@
 
         this.routes = routes.map(function (route) {
           return _objectSpread({
-            pattern: new UrlPattern__default['default'](base + route.path, options),
+            pattern: new UrlPattern__default["default"](base + route.path, options),
             meta: {}
           }, route);
-        });
-        this.route = {
-          key: 0
-        };
-
-        this._match();
-
-        window.addEventListener('popstate', function () {
-          return _this._match();
         });
       }
 
       _createClass(Router, [{
+        key: "_init",
+        value: function _init() {
+          var _this = this;
+
+          this.route = {
+            key: 0
+          };
+
+          this._match(); // Note that because of how Vue's `reactive` works, we can't add an event listener in the constructor,
+          // since `this` in the callback would refer to the original object without reactivity.
+          // https://stackoverflow.com/questions/67894487/vue-3-reactivity-not-triggered-from-inside-a-class-instance
+
+
+          window.addEventListener('popstate', function () {
+            return _this._match();
+          });
+        }
+      }, {
         key: "_match",
         value: function _match() {
           var params;
@@ -292,36 +140,40 @@
       return Router;
     }();
 
-    Router.install = function (Vue) {
-      var router;
-      Vue.mixin({
-        beforeCreate: function beforeCreate() {
-          if (this.$options.router) {
-            router = Vue.observable(this.$options.router);
-          }
-        },
-        methods: {
-          $url: function $url(name, params) {
-            return router.resolve(name, params);
-          }
-        }
-      });
-      Object.defineProperties(Vue.prototype, {
-        $router: {
-          get: function get() {
-            return router;
-          }
-        },
-        $route: {
-          get: function get() {
-            return router.route;
-          }
-        }
-      });
-      Vue.component('router-link', __vue_component__$1);
-      Vue.component('router-view', __vue_component__);
-    };
+    function createRouter(options) {
+      var router = vue.shallowReactive(new Router(options));
 
-    return Router;
+      router._init();
 
-})));
+      router.install = function (app) {
+        app.mixin({
+          methods: {
+            $url: function $url(name, params) {
+              return router.resolve(name, params);
+            }
+          }
+        });
+        Object.defineProperties(app.config.globalProperties, {
+          $router: {
+            get: function get() {
+              return router;
+            }
+          },
+          $route: {
+            get: function get() {
+              return router.route;
+            }
+          }
+        });
+        app.component('router-link', script$1);
+        app.component('router-view', script);
+      };
+
+      return router;
+    }
+
+    exports.createRouter = createRouter;
+
+    Object.defineProperty(exports, '__esModule', { value: true });
+
+}));
