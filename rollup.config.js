02484fbe42ad76bacdde8c3cc882da536ce733e8
Adam Sołtysik <adam27.sol@gmail.com>
2022-07-27 17:48:00 +0200

    Generate output file in 3 separate formats instead of UMD
    Using the UMD package with Webpack could significantly increase the bundle size because of Vue dependency.

--- rollup.config.js
+++ rollup.config.js
@@ -4,15 +4,25 @@ import vue from 'rollup-plugin-vue';
 
 export default {
     input: 'src/index.js',
-    output: {
-        file: 'dist/vue-pocket-router.js',
-        format: 'umd',
-        name: 'VuePocketRouter',
-        globals: {
-            'url-pattern': 'UrlPattern',
-            'vue': 'Vue',
+    output: [
+        {
+            file: 'dist/vue-pocket-router.cjs.js',
+            format: 'cjs',
         },
-    },
+        {
+            file: 'dist/vue-pocket-router.esm.js',
+            format: 'esm',
+        },
+        {
+            file: 'dist/vue-pocket-router.global.js',
+            format: 'iife',
+            name: 'VuePocketRouter',
+            globals: {
+                'url-pattern': 'UrlPattern',
+                'vue': 'Vue',
+            },
+        },
+    ],
     plugins: [
         babel({ babelHelpers: 'inline' }),
         vue(),
